89334a3697831417ed417823e8ebc6b4baab6bce
Danilo Salvador <danilo.salvador@micropower.com.br>
2021-04-01 17:17:20 -0300

    Inserindo transação na API

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transactions: Model,
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
       return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 14400,
-          type: 'deposit',
-          category: 'Salary',
-          createdAt: new Date(),
-        }
-      ]
-    })
-  }
+        this.schema.all('transactions')
+      ];
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transactions', data);
+    });
+  },
 });
 
 ReactDOM.render(
